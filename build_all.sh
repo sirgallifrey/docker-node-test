0754a0b158f8110b621880ada3c90ee858a4b5ce
Adilson Schmitt Junior <adilsonschj@gmail.com>
2017-04-12 19:25:59 -0300

    little build script

---
+++ build_all.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+#
+# Run a test build for all images.
+
+set -uo pipefail
+IFS=$'\n\t'
+
+info() {
+  printf "%s\n" "$@"
+}
+
+fatal() {
+  printf "**********\n"
+  printf "%s\n" "$@"
+  printf "**********\n"
+  exit 1
+}
+
+cd $(cd ${0%/*} && pwd -P);
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+  versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+for version in "${versions[@]}"; do
+  # Skip "docs" and other non-docker directories
+  [ -f "$version/Dockerfile" ] || continue
+
+  tag=$(cat $version/Dockerfile | grep "ENV NODE_VERSION" | cut -d' ' -f3)
+
+  info "Building $tag..."
+  docker build -t node:$tag $version
+
+  if [[ $? -gt 0 ]]; then
+    fatal "Build of $tag failed!"
+  else
+    info "Build of $tag succeeded."
+  fi
+
+  variants=$(echo $version/*/ | xargs -n1 basename)
+
+  for variant in $variants; do
+    # Skip non-docker directories
+    [ -f "$version/$variant/Dockerfile" ] || continue
+    
+    info "Building $tag-$variant variant..."
+    docker build -t node:$tag-$variant $version/$variant
+
+    if [[ $? -gt 0 ]]; then
+      fatal "Build of $tag-$variant failed!"
+    else
+      info "Build of $tag-$variant succeeded."
+    fi
+
+  done
+
+done
+
+info "All builds successful!"
+
+exit 0
